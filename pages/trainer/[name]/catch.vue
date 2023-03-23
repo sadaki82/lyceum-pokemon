6bed5e6a2239c63e3d58ec71220a669ad23a566d
sadakiawada <spark3xxx@gmail.com>
2023-03-23 12:05:23 +0900

    add catch.vue in name

---
+++ pages/trainer/[name]/catch.vue
@@ -0,0 +1,42 @@
+<script setup>
+const config = useRuntimeConfig();
+const page = ref(0);
+const limit = ref(20);
+const offset = computed(() => page.value * limit.value);
+const { data: pokemons, refresh } = await useFetch(
+  () =>
+    `http://pokeapi.co/api/v2/pokemon?offset=${offset.value}&limit=${limit.value}`
+);
+const hasPrev = computed(() => page.value > 0);
+const maxPage = computed(() => Math.floor(pokemons.value.count / limit.value));
+const hasNext = computed(() => page.value < maxPage.value);
+const onPrev = async () => {
+  page.value--;
+  await refresh();
+};
+const onNext = async () => {
+  page.value++;
+  await refresh();
+};
+</script>
+
+<template>
+  <div>
+    <h1>ポケモンをつかまえる</h1>
+    <p>{{ pokemons.count }}種類のポケモン</p>
+    <p>{{ page + 1 }}/{{ maxPage + 1 }}ページ</p>
+    <GamifyList>
+      <GamifyItem v-for="pokemon in pokemons.results" :key="pokemon.url">
+        <span class="pokemon-name">{{ pokemon.name }}</span>
+      </GamifyItem>
+    </GamifyList>
+    <GamifyList direction="horizon">
+      <GamifyItem>
+        <GamifyButton :disabled="!hasPrev" @click="onPrev">前へ</GamifyButton>
+      </GamifyItem>
+      <GamifyItem>
+        <GamifyButton :disabled="!hasNext" @click="onPrev">次へ</GamifyButton>
+      </GamifyItem>
+    </GamifyList>
+  </div>
+</template>
